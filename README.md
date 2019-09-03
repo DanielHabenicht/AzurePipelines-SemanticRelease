2def5408361346ddfb66740d2cbb01029cb477cd
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-09-04 01:29:38 +0200

    fix(semanticRelease): setup build and first draft

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Semantic Release Task for Azure Pipelines
 
-... 
+This is a Azure DevOps Extension that supplies you with multiple Azure Piplines Tasks that make your release much easier.
+
+Provide multiple Tasks:
+
+- Semantic Release Freestyle (using a `releaserc` file)
+- Pre-Defined Tasks:
+  - NPM Package
+  - Just Release Notes
+- Create an issue to add your idea.
---
+++ updateVersionNumbers.js
@@ -0,0 +1,29 @@
+/**
+ * Updates the Version number during a release with semantic release
+ * Usage:
+ * node updateVersionNumber.js <version-string>
+ */
+
+const { resolve, relative } = require('path');
+const fs = require('fs');
+version = process.argv[2];
+
+// Update vss-extension.json
+
+const file1 = resolve(__dirname, 'src', 'vss-extension.json');
+const vssextension = fs.readFileSync(file1, { encoding: 'utf8' });
+fs.writeFileSync(file1, vssextension.replace(/(version": ")(.*)(")/g, `$1${version}$3`), { encoding: 'utf-8' });
+
+// Update task.json
+const file2 = resolve(__dirname, 'src', 'semanticRelease', 'task.json');
+const task = fs.readFileSync(file2, { encoding: 'utf8' });
+fs.writeFileSync(
+  file2,
+  task
+    .replace(/("Major": )(\d*)/g, `$1${version.split('.')[0]}`)
+    .replace(/("Minor": )(\d*)/g, `$1${version.split('.')[1]}`)
+    .replace(/("Patch": )(\d*)/g, `$1${version.split('.')[2]}`),
+  { encoding: 'utf-8' }
+);
+
+console.log(`Wrote version info ${version} to ${relative(resolve(__dirname, '..'), file2)}`);
