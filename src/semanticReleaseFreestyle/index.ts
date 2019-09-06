4c94cb6be65b0e7e3df19e525781b011e85f37b3
Daniel Habenicht <daniel-habenicht@outlook.de>
2019-09-06 22:22:35 +0200

    fix(test): try paths

--- src/semanticReleaseFreestyle/index.ts
+++ src/semanticReleaseFreestyle/index.ts
@@ -1,7 +1,9 @@
 import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner');
 import { Utility } from './utility/utility';
-const semanticReleaseFreestyle = require('semantic-release');
+const semanticRelease = require('semantic-release');
 const fs = require('fs');
+import * as path from 'path';
 
 export enum ConfigType {
   filePath = 'filePath',
@@ -11,6 +13,18 @@ export enum ConfigType {
 // Look for other parts of this extension here: $(Work_Folder)/_tasks/semanticReleaseFreestyleAzureTask/...
 
 async function run() {
+  const localFolder = path.join(__dirname);
+  console.log(`Skript Folder: ${localFolder}`);
+  console.log(`CWD: ${process.cwd}`);
+
+  const npm = new tr.ToolRunner('npm');
+  npm.line('install');
+  npm.exec().then(async () => {
+    await runSemanticRelease();
+  });
+}
+
+async function runSemanticRelease() {
   try {
     const configType: ConfigType = tl.getInput('configType', true) as ConfigType;
 
@@ -23,16 +37,14 @@ async function run() {
     }
     const githubEndpoint = tl.getInput('gitHubServiceName', true);
     const githubEndpointToken = Utility.getGithubEndPointToken(githubEndpoint);
-    process.env.GH_TOKEN = githubEndpointToken;
 
-    const result = await semanticReleaseFreestyle(
+    const result = await semanticRelease(
       { ...semanticReleaseFreestyleOption },
       {
         // Run semantic-release from `/path/to/git/repo/root` without having to change local process `cwd` with `process.chdir()`
         cwd: tl.getInput('cwd'),
         // Pass the variable `MY_ENV_VAR` to semantic-release without having to modify the local `process.env`
         env: { ...process.env, GH_TOKEN: githubEndpointToken },
-        // Store stdout and stderr to use later instead of writing to `process.stdout` and `process.stderr`
         stdout: process.stdout,
         stderr: process.stderr
       }
@@ -41,19 +53,16 @@ async function run() {
     if (result) {
       const { lastRelease, commits, nextRelease, releases } = result;
 
-      console.log(
+      tl.setResult(
+        tl.TaskResult.Succeeded,
         `Published ${nextRelease.type} release version ${nextRelease.version} containing ${commits.length} commits.`
       );
-
-      if (lastRelease.version) {
-        console.log(`The last release was "${lastRelease.version}".`);
-      }
 
       for (const release of releases) {
         console.log(`The release was published with plugin "${release.pluginName}".`);
       }
     } else {
-      console.log('No release published.');
+      tl.setResult(tl.TaskResult.Succeeded, 'No release published.');
     }
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message);
